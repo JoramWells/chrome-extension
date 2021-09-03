0d691e3a8d91358bb4881f56eddaf0351de0e001
bramuel <jorammanoah1@gmail.com>
2021-09-03 13:36:37 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import { vocab } from "./vocabList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Typewriter from 'typewriter-effect'
 
 function App() {
+  const [data, setData] = useState([]);
+  const fetchData = () => {
+    axios
+      .get("https://allquotes.herokuapp.com/quotes/rand")
+      .then((response) => {
+        setData(response.data);
+        localStorage.setItem('data', JSON.stringify(response.data))
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  console.log(JSON.parse(localStorage.getItem('data'))['author'])
+
+  useEffect(() => {
+    fetchData();
+    return () => {};
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="banner" >
+      {/* <div style={{color:"white"}} className="code">
+       <pre id="typewriter">
+{`         #include stdio.h
+            int main(){
+              printf("Hello world!!")
+            }
+`}
+         
+       </pre>
+      </div> */}
+      <div className="container">
+
+        
+        <Typewriter
+      onInit = {(typewriter)=>{
+        typewriter.typeString(JSON.parse(localStorage.getItem('data'))['text']).start()
+      }}
+       />
+        {/* <p>{JSON.parse(localStorage.getItem('data'))['text']}</p> */}
+        <div style={{ color: "white", fontSize:"0.95rem" }}>
+          ~ [<i>{JSON.parse(localStorage.getItem('data'))['author'] ? JSON.parse(localStorage.getItem('data'))['author']: 'Anonymous'}</i>]
+        </div>
+      </div>
     </div>
   );
 }
