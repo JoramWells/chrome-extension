413616054ceb990552562ac4bad8283f052208e8
bramuel <jorammanoah1@gmail.com>
2021-09-03 15:47:55 +0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -2,53 +2,58 @@ import "./App.css";
 // import { vocab } from "./vocabList";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Typewriter from 'typewriter-effect'
+import Typewriter from "typewriter-effect";
+import { HeartIcon, SunIcon } from "@heroicons/react/solid";
 
 function App() {
   const [data, setData] = useState([]);
-  const fetchData = () => {
-    axios
+  const fetchData = async () => {
+    await axios
       .get("https://allquotes.herokuapp.com/quotes/rand")
       .then((response) => {
         setData(response.data);
-        localStorage.setItem('data', JSON.stringify(response.data))
+        localStorage.setItem("data", JSON.stringify(response.data));
       })
       .catch((err) => {
         console.log(err);
       });
   };
-  console.log(JSON.parse(localStorage.getItem('data'))['author'])
 
   useEffect(() => {
     fetchData();
     return () => {};
   }, []);
   return (
-    <div className="banner" >
-      {/* <div style={{color:"white"}} className="code">
-       <pre id="typewriter">
-{`         #include stdio.h
-            int main(){
-              printf("Hello world!!")
-            }
-`}
-         
-       </pre>
-      </div> */}
-      <div className="container">
-
-        
-        <Typewriter
-      onInit = {(typewriter)=>{
-        typewriter.typeString(JSON.parse(localStorage.getItem('data'))['text']).start()
-      }}
-       />
-        {/* <p>{JSON.parse(localStorage.getItem('data'))['text']}</p> */}
-        <div style={{ color: "white", fontSize:"0.95rem" }}>
-          ~ [<i>{JSON.parse(localStorage.getItem('data'))['author'] ? JSON.parse(localStorage.getItem('data'))['author']: 'Anonymous'}</i>]
+    <>
+      <div className="banner">
+        <div className="container">
+          {/* <div >
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(`<h1 style="color:#50c878" >Welcome back Sir,</h1>`)
+                .start();
+            }}
+          />
+        </div> */}
+          <h1 style={{ color: "#50c878" }}>
+            <u>gLad tO hAvE yoU sir</u>
+            <SunIcon style={{ width: "1.5rem", color: "#fff44f" }} />
+          </h1>
+          <p>{data["text"]}</p>
+          <div style={{ color: "white", fontSize: "0.9rem", margin: "10px" }}>
+            ~ [<i>{data["author"] ? data["author"] : "Anonymous"}</i>]
+            <div>
+              <HeartIcon style={{ width: "1.5rem", color: "#f88379" }} />
+            </div>
+          </div>
         </div>
+      <footer className="footer">
+       Copyright @2021
+      </footer>
       </div>
-    </div>
+
+    </>
   );
 }
 
